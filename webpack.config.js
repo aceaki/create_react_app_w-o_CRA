832d0f6916fba82526eb56931ae6529f5e15a0d8
alex <alexceaki@gmail.com>
2020-08-11 21:34:52 +0100

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,21 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
+  entry: './src/index.jsx',
   resolve: {
     extensions: ['.js', '.jsx']
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundlefile.js'
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
-        loader: 'babel-loader'
+        loader: 'babel-loader',
+        exclude: /node_modules/
       }
     ]
   },
